54faeb4cf55ea701913ae797f2bba6d6de453547
Jedrzej Dzierzak <djsahiu@interia.pl>
2023-03-30 21:47:41 +0200

    second commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const table = document.querySelector("table");
 const tdElements = [...document.querySelectorAll("td")];
 
 let char = undefined;
-let enemyChar = undefined;
+let charFromTheBoard = undefined;
 let ws = undefined;
 
 btnPlay.addEventListener("click", () => {
@@ -13,90 +13,49 @@ btnPlay.addEventListener("click", () => {
   table.style.display = "block";
 
   ws.addEventListener("message", (data) => {
-    console.log(data);
-    console.log("Weszło do message!");
     const parsedData = JSON.parse(data.data);
 
-    char = parsedData.data.char;
-
-    /*
-    boardState = parsedData.data;
-    
-    for (let i = 0; i < boardState.length; i++) {
-      for (let j = 0; j < boardState[0].length; j++) {
-        if (boardState[i][j] != null) {
-          console.log("ok");
-          tdElements.forEach((td) => {
-            if (
-              td.getAttribute("data-x") == i &&
-              td.getAttribute("data-y") == j
-            ) {
-              let positionX = td.getAttribute("data-x");
-              let positionY = td.getAttribute("data-y");
-              let position = {
-                messageType: messageTypes.ClientSaysLastMove,
-                data: {
-                  positionX,
-                  positionY,
-                  whatClicked: "X",
-                },
-              };
-              ws.send(JSON.stringify(position));
-              td.innerText = char;
-            }
-          });
+    if (parsedData.messageType == messageTypes.ServerSaysAssignPlayerChar) {
+      char = parsedData.data.char;
+    } else if (parsedData.messageType == messageTypes.ServerSaysUpdatedBoard) {
+      boardState = parsedData.data;
+      console.log(boardState);
+      for (let i = 0; i < boardState.length; i++) {
+        for (let j = 0; j < boardState[0].length; j++) {
+          if (boardState[i][j] != undefined) {
+            charFromTheBoard = boardState[i][j];
+            tdElements.forEach((td) => {
+              if (
+                td.getAttribute("data-x") == j &&
+                td.getAttribute("data-y") == i
+              ) {
+                td.innerText = charFromTheBoard; // przechodzi po całej tablicy przeciwnika i podstawia zebrane znaki
+              }
+            });
+          }
         }
       }
     }
-    */
-
-    const updatedBoard = parsedData.data;
-
-    for (let i = 0; i < updatedBoard.length; i++) {
-      for (let j = 0; j < updatedBoard[0].length; j++) {
-        if (updatedBoard[i][j] != null) {
-          enemyChar = updatedBoard[i][j];
-          console.log(char);
-          tdElements.forEach((td) => {
-            if (
-              td.getAttribute("data-x") == i &&
-              td.getAttribute("data-y") == j
-            ) {
-              td.innerText = enemyChar;
-            }
-          });
-        }
-      }
-    }
-
-    console.log(parsedData);
-    console.log(parsedData.data);
-    console.log(char); // char robi się undefined u drugiej osoby po pierwszym kliknięciu pierwszej osoby w którąkolwiek komórkę planszy. Wynika to z tego że obiekt data podany jako parametr zmienia się na inny obiekt za każdym razem
   });
 });
 
 tdElements.forEach((td) =>
   td.addEventListener("click", (ev) => {
-    console.log(ev.target);
-    let positionX = Number(ev.target.getAttribute("data-x")); // zakładamy że 2
-    let positionY = Number(ev.target.getAttribute("data-y")); // zakładamy że 2
-
-    console.log(char); // X
+    let positionX = Number(ev.target.getAttribute("data-y"));
+    let positionY = Number(ev.target.getAttribute("data-x"));
 
     let position = {
-      messageType: messageTypes.ClientSaysLastMove, // 1
+      messageType: messageTypes.ClientSaysLastMove,
       data: {
-        positionX, // zakładamy że 2
-        positionY, // zakładamy że 2
-        whatClicked: char, // X
+        positionX,
+        positionY,
+        whatClicked: char,
       },
     };
 
-    console.log(positionX);
-    console.log(positionY);
-    ev.target.innerText = char; // stawiamy X w pole które klikneliśmy
-    console.log(boardState);
-    ws.send(JSON.stringify(position)); // tutaj leci do serwera po pierwszym kliknięciu
+    ev.target.innerText = char;
+
+    ws.send(JSON.stringify(position));
   })
 );
 
